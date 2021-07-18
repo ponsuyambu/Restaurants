a5b9972bc21070a46f58df4ab8cac8106e207c2f
Pons <suyambu.vps@gmail.com>
2021-07-18 17:18:33 +0200

    chore: add sample list for restaurants

--- feature-restaurant-list/presentation/src/main/java/challenge/android/feature/restaurants/presentation/viewmodel/RestaurantsViewModel.kt
+++ feature-restaurant-list/presentation/src/main/java/challenge/android/feature/restaurants/presentation/viewmodel/RestaurantsViewModel.kt
@@ -2,9 +2,12 @@ package challenge.android.feature.restaurants.presentation.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import challenge.android.common.utils.BaseViewModel
 import challenge.android.feature.restaurants.presentation.uimodels.RestaurantUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,7 +16,18 @@ class RestaurantsViewModel @Inject constructor() : BaseViewModel() {
     private val restaurants = mutableListOf<RestaurantUiModel>()
     private val showRestaurantsList = MutableLiveData<Boolean>()
 
-    fun requestRestaurants() {}
+    fun requestRestaurants() {
+        viewModelScope.launch {
+            showProgress.value = true
+            delay(3000)
+            restaurants.apply {
+                add(RestaurantUiModel("Restaurant 1", 4f))
+                add(RestaurantUiModel("Restaurant 2", 3f))
+            }
+            showRestaurantsList.postValue(true)
+            showProgress.value = false
+        }
+    }
 
     fun restaurants(): List<RestaurantUiModel> = restaurants
     fun showRestaurantsList(): LiveData<Boolean> = showRestaurantsList
