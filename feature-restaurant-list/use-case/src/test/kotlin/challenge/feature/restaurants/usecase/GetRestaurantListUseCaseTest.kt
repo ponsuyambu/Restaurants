b27533d4513e581925beeeebc5312c13702c1f24
Pons <suyambu.vps@gmail.com>
2021-07-19 00:35:17 +0200

    chore: simplify use case test

--- feature-restaurant-list/use-case/src/test/kotlin/challenge/feature/restaurants/usecase/GetRestaurantListUseCaseTest.kt
+++ feature-restaurant-list/use-case/src/test/kotlin/challenge/feature/restaurants/usecase/GetRestaurantListUseCaseTest.kt
@@ -2,11 +2,13 @@ package challenge.feature.restaurants.usecase
 
 import challenge.android.testutils.TestCoroutineRule
 import challenge.feature.restaurants.data.RestaurantRepository
+import challenge.feature.restaurants.domain.Restaurant
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
@@ -32,11 +34,12 @@ class GetRestaurantListUseCaseTest {
 
     @Test
     fun `should call repository when use case is invoked`() = testCoroutineRule.runBlockingTest {
-        coEvery { repository.getRestaurants() } returns listOf(RESTAURANT_1, RESTAURANT_2)
+        val restaurantList = mockk<List<Restaurant>>()
+        coEvery { repository.getRestaurants() } returns restaurantList
 
         val restaurants = getRestaurantListUseCase()
 
         coVerify { repository.getRestaurants() }
-        assertEquals(listOf(RESTAURANT_1, RESTAURANT_2), restaurants)
+        assertEquals(restaurantList, restaurants)
     }
 }
